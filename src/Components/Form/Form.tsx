c01d49cf527c1686219483aba7988b5bfa710c7a
lokasan <lokasan@inbox.ru>
2023-09-13 20:19:44 +0300

    fixed a useCallback and a useEffect

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -15,14 +15,14 @@ export const Form = () => {
             subject
         }
         tg.sendData(JSON.stringify(data))
-    }, [])
+    }, [country, street, subject])
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
         return () => {
             tg.offEvent('mainButtonClicked', onSendData)
         }
-    }, [])
+    }, [onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
